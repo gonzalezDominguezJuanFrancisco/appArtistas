df0586482ccfe0362f294cc1079522da3c4f79ab
unknown <juanfgonzalez19@iespuntadelverde.es>
2021-02-28 16:59:16 +0100

    Undecima actualizacion

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const app = express()
 app.use(express.static('./dist/'));
 app.get('/*', function(req, res) {
- res.sendFile('index.html', {root: 'dist/porcanciones3/'}
+ res.sendFile('index.html', {root: 'dist/procanciones3/'}
  );
 });
 const port = 3500;
